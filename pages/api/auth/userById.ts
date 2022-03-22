ba9dbc4ab5b3a4031c3029bfdcaf2ec538592fa0
chibuike-vincent <chibuikepatrick2@gmail.com>
2022-03-22 10:40:12 +0100

    fixes

--- pages/api/auth/userById.ts
+++ pages/api/auth/userById.ts
@@ -11,7 +11,6 @@ export default async(req: NextApiRequest, res:NextApiResponse) =>{
     }
 
     const user:any = req.query
-    console.log(user.id, "dataddddd")
 
     const response = await prisma.user.findUnique({
         where: {
